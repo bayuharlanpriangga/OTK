e7535081934c3d7d4e26edff46919dab35ab5fc8
Bayu Harlan Priangga <bayuharlanpriangga@gmail.com>
2026-06-14 00:57:42 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -10,8 +10,8 @@ const CACHE_NAME = 'otk-cache';
 const PRECACHE_ASSETS = [
   './index.html',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
+  './icon-192.png',
+  './icon-512.png',
 ];
 
 self.addEventListener('install', (event) => {
